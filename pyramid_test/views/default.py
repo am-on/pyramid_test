0bc55f7fe5e6d706e145e61a478c2ac9bc2195c2
Amon Stopinšek <igre@outlook.com>
2017-07-05 20:32:58 +0200

    add basic models and views

--- pyramid_test/views/default.py
+++ pyramid_test/views/default.py
@@ -1,34 +1,24 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 
-from sqlalchemy.exc import DBAPIError
+from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import HTTPNotFound
 
 from ..models import Comment
 from ..models import Post
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.pt')
+@view_config(route_name='home', renderer='../templates/posts.pt')
 def my_view(request):
-    try:
-        query = request.dbsession.query(Post)
-        one = query.filter(Post.title == 'Example post').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'blog'}
+    posts = request.dbsession.query(Post).order_by(Post.id.desc()).all()
+    return {'posts': posts}
 
 
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+@view_config(route_name='post', renderer='../templates/singlepost.pt')
+def single_post(request):
+    post_id = request.matchdict['post_id']
+    post = request.dbsession.query(Post).filter_by(id=post_id).first()
+    if post is None:
+        raise HTTPNotFound('No such page')
 
-1.  You may need to run the "initialize_pyramid_test_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+    return {'post': post}
